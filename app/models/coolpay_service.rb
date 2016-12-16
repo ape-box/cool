ecb8ee401a3e801b3bc245b19b47976d68712ba8
Alessio Peternelli <alessio.peternelli@gmail.com>
2016-12-16 21:58:50 +0000

    Cleaned up CoolpayService and added tests

--- app/models/coolpay_service.rb
+++ app/models/coolpay_service.rb
@@ -7,77 +7,61 @@ class CoolpayService
 
   # login ----
   def login(username, api_key)
-    raise "Error, invalid username" if username.blank? || !username.is_a?(String)
-    raise "Error, invalid api_key" if api_key.blank? || !api_key.is_a?(String)
-
     url = @uri + '/login'
     params = {'username' => username, 'apikey' => api_key}
     headers = {'content_type' => 'json', 'accept' => 'json'}
 
     response = @rest_client.post(url, params, headers)
-
-    obj = ActiveSupport::JSON.decode response.body
-    obj['token']
+    return_body_content response, 'token'
   end
 
   # recipients ----
-  def list_recipients(token, filter_by_name=nil)
-    raise "Error, invalid token" if token.blank? || !token.is_a?(String)
-
+  def list_recipients(filter_by_name=nil)
     url = @uri + '/recipients'
     params = {'name' => filter_by_name}
-    # i would expect all the application to be protected ...
-    #headers = {'accept' => 'json', "authorization" => "Bearer 12345.#{token}.67890"}
-    headers = {'accept' => 'json'}
 
-    response = @rest_client.get(url, {'params' => params, 'headers' => headers})
-
-    obj = ActiveSupport::JSON.decode response.body
-    obj['recipients']
+    response = @rest_client.get(url, {'params' => params, 'headers' => {'accept' => 'json'}})
+    return_body_content response, 'recipients'
   end
 
   def create_recipient(token, recipient_name)
-    raise "Error, invalid token" if token.blank? || !token.is_a?(String)
-    raise "Error, invalid recipient_name" if recipient_name.blank? || !recipient_name.is_a?(String)
-
     url = @uri + '/recipients'
     params = {'recipient' => {'name' => recipient_name}}
-    headers = {'content_type' => 'json', 'accept' => 'json', "authorization" => "Bearer 12345.#{token}.67890"}
 
-    response = @rest_client.post(url, {'params' => params, 'headers' => headers})
-
-    obj = ActiveSupport::JSON.decode response.body
-    obj['recipient']
+    response = @rest_client.post(url, {'params' => params, 'headers' => headers_post(token)})
+    return_body_content response, 'recipient'
   end
 
   # payments ----
   def list_payments(token)
-    raise "Error, invalid token" if token.blank? || !token.is_a?(String)
-
     url = @uri + '/payments'
-    headers = {'accept' => 'json', "authorization" => "Bearer 12345.#{token}.67890"}
-
-    response = @rest_client.get(url, {'headers' => headers})
-
-    obj = ActiveSupport::JSON.decode response.body
-    obj['payments']
+    response = @rest_client.get(url, {'headers' => headers_get(token)})
+    return_body_content response, 'payments'
   end
 
   def create_payment(token, ammount_bigdecimal, currency, recipient_id)
-    raise "Error, invalid token" if token.blank? || !token.is_a?(String)
-    raise "Error, invalid ammount_bigdecimal" if ammount_bigdecimal.blank? || !ammount_bigdecimal.is_a?(BigDecimal)
-    raise "Error, invalid currency" if currency.blank? || !currency.is_a?(String)
-    raise "Error, invalid recipient_id" if recipient_id.blank? || !recipient_id.is_a?(String)
+    raise 'Error, invalid ammount_bigdecimal' if ammount_bigdecimal.blank? || !ammount_bigdecimal.is_a?(BigDecimal)
 
     url = @uri + '/payments'
-    params = { "payment" => {"amount" => ammount_bigdecimal, "currency" => currency, "recipient_id" => recipient_id}}
-    headers = {'content_type' => 'json', 'accept' => 'json', "authorization" => "Bearer 12345.#{token}.67890"}
+    params = { 'payment' => {'amount' => ammount_bigdecimal, 'currency' => currency, 'recipient_id' => recipient_id}}
 
-    response = @rest_client.post(url, {'params' => params, 'headers' => headers})
-
-    obj = ActiveSupport::JSON.decode response.body
-    obj['payments']
+    response = @rest_client.post(url, {'params' => params, 'headers' => headers_post(token)})
+    return_body_content response, 'payment'
   end
 
+  private
+
+    def headers_post(token)
+      {'content_type' => 'json', 'accept' => 'json', 'authorization' => "Bearer 12345.#{token}.67890"}
+    end
+
+    def headers_get(token)
+      {'accept' => 'json', 'authorization' => "Bearer 12345.#{token}.67890"}
+    end
+
+    def return_body_content(response, namespace=nil)
+      obj = ActiveSupport::JSON.decode response.body
+      namespace.nil? ? obj : obj[namespace]
+    end
 
 end
